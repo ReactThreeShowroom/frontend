6b9db66ab2b4c935a9f49e152f86ffe46c210138
jollyren <jollyren@gmail.com>
2023-09-23 17:32:51 -0600

    fixed signin comp - sign out link broken, now fixed

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom'
+import { handleNavClick } from '../utils/eventHandlers'
+import { useSelector, useDispatch } from 'react-redux'
 
 const SignIn = () => {
+  const nav = useSelector((state) => state.nav)
+  const dispatch = useDispatch()
+
+  const [link] = nav.filter((navLink) => navLink.path == '/signup')
+  const isSelected = link.selected ? 'text-main-orange ' : ''
+  const isHidden = !link.active ? 'hidden ' : ''
+  const navLinkStyles = `${isSelected}${isHidden}`
+
   return (
-    <div>
-      {/* form goes here for login, option to signup if you don't have a user */}
-      <Link to={'/signup'} id={'pageSignupLink'} className={'normalLink'}>
+    <>
+      <Link
+        to={link.path}
+        id={`page${link.value}`}
+        className={navLinkStyles}
+        onClick={() => handleNavClick(dispatch, link)}>
         Sign Up
       </Link>
-    </div>
+    </>
   )
 }
 
