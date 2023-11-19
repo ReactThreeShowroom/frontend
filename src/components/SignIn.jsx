069b9a870d8662feff5e295d391845e9aaa711a7
jollyren <jollyren@gmail.com>
2023-11-18 20:31:02 -0700

    did some styling and functional changes in SignUp, SignIn, PasswordReset, added missing components to /components/index.js

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,86 +1,81 @@
-import { Link } from "react-router-dom";
-import { handleNavClick } from "../utils/eventHandlers";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { handleNavClick } from '../utils/eventHandlers'
+import { useSelector, useDispatch } from 'react-redux'
 
 const SignIn = () => {
-  const nav = useSelector((state) => state.nav);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const dispatch = useDispatch()
+  const [formState, setFormState] = useState({
+    email: '',
+    password: ''
+  })
 
-  const [link] = nav.filter((navLink) => navLink.path == "/signup");
-  const isSelected = link.selected ? "text-main-orange " : "";
-  const isHidden = !link.active ? "hidden " : "";
-  const navLinkStyles = `${isSelected}${isHidden} hover:bg-main-orange hover:text-[#ffffff]`
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log('loggin in...')
+    // do login stuff
+    // if (success) { navigate('/account')}
+    // if (error) { setMessage(error.message)}
+    setFormState({ email: '', password: '' }) //temp to test behavior
+  }
 
-  const handleLogin = () => {
-    console.log("loggin in...");
-  };
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.id]: e.target.value })
+  }
 
   return (
-    // <>
-    //   <Link
-    //     to={link.path}
-    //     id={`page${link.value}`}
-    //     className={navLinkStyles}
-    //     onClick={() => handleNavClick(dispatch, link)}>
-    //     Sign Up
-    //   </Link>
-    // </>
-    <div className="flex justify-center h-screen w-screen items-center flex-col">
-      <div className="text-grey-darker text-md font-bold mb-2 underline">
-        Log In
-      </div>
+    <form
+      className="flex justify-center h-screen w-screen items-center flex-col"
+      onSubmit={handleLogin}>
+      <p className="text-grey-darker text-md font-bold mb-2 underline">Log In</p>
       <div className="w-full md:w-1/2 flex flex-col items-center ">
         <div className="mb-4">
-          <label
-            className="block text-grey-darker text-sm font-bold mb-2"
-            htmlFor="Email"
-          >
+          <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="email">
             Email
           </label>
           <input
-            className="shadow border rounded w-full py-2 px-3 text-grey-darker mb-3"
-            id="Email"
-            name="Email"
-            type="text"
-            placeholder="Email"
+            className="shadow-sm border-2 rounded-md w-full py-2 px-3 text-grey-darker mb-3"
+            id="email"
+            name="email"
+            autoComplete="email"
+            type="email"
+            onChange={handleChange}
+            value={formState.email}
+            placeholder="email@domain.com"
           />
         </div>
         <div className="mb-3">
-          <label
-            className="block text-grey-darker text-sm font-bold mb-2"
-            htmlFor="password"
-          >
+          <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
             Password
           </label>
           <input
-            className="shadow border rounded w-full py-2 px-3 text-grey-darker mb-3"
+            className="shadow-sm border-2 rounded-md w-full py-2 px-3 text-grey-darker mb-3"
             id="password"
+            autoComplete="password"
             name="password"
             type="password"
+            onChange={handleChange}
+            value={formState.password}
             placeholder="******************"
           />
         </div>
       </div>
-      <div className="flex items-center justify-between flex-col">
+      <div className="flex flex-col items-center justify-between">
         <button
-          className="bg-main-orange w-full my-2 rounded"
-          type="button"
-          onClick={handleLogin}
-        >
+          className="bg-transparent text-main-orange hover:bg-main-orange hover:text-white border-2 border-main-orange w-full p-2 my-2 rounded-md text-center"
+          type="submit">
           Log In
         </button>
-        <a
-          className=" font-bold text-sm"
-          href="#"
-          onClick={() => navigate(`/passwordReset/`)}
-        >
+        <Link
+          to="/passwordReset"
+          id="signInResetLink"
+          className="bg-transparent hover:text-main-orange w-full p-2 my-2 rounded-md text-center"
+          onClick={() => handleNavClick(dispatch, '/passwordReset')}>
           Forgot Password?
-        </a>
+        </Link>
       </div>
-    </div>
-  );
-};
+    </form>
+  )
+}
 
-export default SignIn;
+export default SignIn
