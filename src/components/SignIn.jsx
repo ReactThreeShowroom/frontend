160cdee2e8acc1a2fd12f8c455d189d97626cfe1
Jeremy Rogers <93287920+JollyRen@users.noreply.github.com>
2023-11-18 18:25:33 -0700

    Update SignIn.jsx
    fixed class => className, for => htmlFor, added name="" to inputs for the labels
    
    looking at the <a> tag, as I'm not 100% sure what it's doing, looks like a nav to another page

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -11,7 +11,7 @@ const SignIn = () => {
   const [link] = nav.filter((navLink) => navLink.path == "/signup");
   const isSelected = link.selected ? "text-main-orange " : "";
   const isHidden = !link.active ? "hidden " : "";
-  const navLinkStyles = `${isSelected}${isHidden}`
+  const navLinkStyles = `${isSelected}${isHidden} hover:bg-main-orange hover:text-[#ffffff]`
 
   const handleLogin = () => {
     console.log("loggin in...");
@@ -27,50 +27,52 @@ const SignIn = () => {
     //     Sign Up
     //   </Link>
     // </>
-    <div class="flex justify-center h-screen w-screen items-center flex-col">
+    <div className="flex justify-center h-screen w-screen items-center flex-col">
       <div className="text-grey-darker text-md font-bold mb-2 underline">
         Log In
       </div>
-      <div class="w-full md:w-1/2 flex flex-col items-center ">
-        <div class="mb-4">
+      <div className="w-full md:w-1/2 flex flex-col items-center ">
+        <div className="mb-4">
           <label
-            class="block text-grey-darker text-sm font-bold mb-2"
-            for="Email"
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="Email"
           >
             Email
           </label>
           <input
-            class="shadow border rounded w-full py-2 px-3 text-grey-darker mb-3"
+            className="shadow border rounded w-full py-2 px-3 text-grey-darker mb-3"
             id="Email"
+            name="Email"
             type="text"
             placeholder="Email"
           />
         </div>
-        <div class="mb-3">
+        <div className="mb-3">
           <label
-            class="block text-grey-darker text-sm font-bold mb-2"
-            for="password"
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="password"
           >
             Password
           </label>
           <input
-            class="shadow border rounded w-full py-2 px-3 text-grey-darker mb-3"
+            className="shadow border rounded w-full py-2 px-3 text-grey-darker mb-3"
             id="password"
+            name="password"
             type="password"
             placeholder="******************"
           />
         </div>
       </div>
-      <div class="flex items-center justify-between flex-col">
+      <div className="flex items-center justify-between flex-col">
         <button
-          class="bg-main-orange w-full my-2 rounded"
+          className="bg-main-orange w-full my-2 rounded"
           type="button"
           onClick={handleLogin}
         >
           Log In
         </button>
         <a
-          class=" font-bold text-sm"
+          className=" font-bold text-sm"
           href="#"
           onClick={() => navigate(`/passwordReset/`)}
         >
