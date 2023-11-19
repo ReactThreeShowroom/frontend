225af9014c3896808d09ea52ab08b193b6ca635d
jollyren <jollyren@gmail.com>
2023-11-18 17:35:26 -0700

    added showroom stuff

--- src/components/Showroom.jsx
+++ src/components/Showroom.jsx
@@ -33,6 +33,7 @@ const Showroom = () => {
         <pointLight position={[10, 10, 10]} />
         <Box position={[-1.2, 0, 0]} />
         <Box position={[1.2, 0, 0]} />
+        <OrbitControls />
       </Canvas>
     </section>
   )
