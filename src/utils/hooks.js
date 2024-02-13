adc10ef6ff6c2abe801c029057bb7c8433e96f6b
JollyRen <jollren@gmail.com>
2024-02-13 16:02:42 -0700

    merging main with pwa into stateConversion before merge to main

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 
-export const useNavigateClick = () => {
+export const useNavigateClick = (pathName) => {
   const navigate = useNavigate()
   return () => navigate(pathName)
 }
