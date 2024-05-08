3068d2da8960e731481e54a96ec112f9912564cb
JollyRen <jollren@gmail.com>
2024-05-08 14:27:42 -0600

    fixed 'location not iterable' and classnames and nav / route object/array formatting for readability

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -1,51 +1,16 @@
-import App from "../App";
-import {
-  Admin,
-  Home,
-  Account,
-  SignIn,
-  SignUp,
-  Showroom,
-  PasswordReset,
-} from "../components";
+import App from '../App'
+import { Admin, Home, Account, SignIn, SignUp, Showroom, PasswordReset } from '../components'
 
 const rootChildren = [
-  {
-    index: true,
-    element: <Home />,
-  },
-  {
-    path: "/admin",
-    element: <Admin />,
-  },
-  {
-    path: "/account",
-    element: <Account />,
-  },
-  {
-    path: "/signin",
-    element: <SignIn />,
-  },
-  {
-    path: "/showroom",
-    element: <Showroom />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/passwordReset",
-    element: <PasswordReset />,
-  },
-];
+  { index: true, element: <Home /> },
+  { path: '/admin', element: <Admin /> },
+  { path: '/account', element: <Account /> },
+  { path: '/signin', element: <SignIn /> },
+  { path: '/showroom', element: <Showroom /> },
+  { path: '/signup', element: <SignUp /> },
+  { path: '/passwordReset', element: <PasswordReset /> }
+]
 
-const routes = [
-  {
-    path: "/",
-    element: <App />,
-    children: rootChildren,
-  },
-];
+const routes = [{ path: '/', element: <App />, children: rootChildren }]
 
-export default routes;
+export default routes
