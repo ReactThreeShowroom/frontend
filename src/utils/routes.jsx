084e2e89e41f5ac5e55c1e8aaba6e5188bf1ab06
JollyRen <jollren@gmail.com>
2024-07-04 14:52:46 -0600

    created loader and actions for all functions of a client - update, deactivate, reactivate - basic styles

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -9,12 +9,17 @@ import {
   SignUp,
   SingleClient
 } from '../pages'
-import { fetchUserLoader } from './fetches'
+import { fetchClientLoader, fetchUserLoader, updateClientAction } from './fetches'
 
 const rootChildren = [
   { index: true, element: <Home /> },
   { path: '/account', element: <Account /> },
-  { path: '/client/:clientId', element: <SingleClient /> },
+  {
+    path: '/client/:clientId',
+    element: <SingleClient />,
+    loader: fetchClientLoader,
+    action: updateClientAction
+  },
   { path: '/admin', element: <Admin /> },
   { path: '/passwordReset', element: <PasswordReset /> },
   { path: '/showroom', element: <Showroom /> },
