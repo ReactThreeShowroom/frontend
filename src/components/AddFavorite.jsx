6980853d259b942e67d1045b0730de6b0216663e
JollyRen <jollren@gmail.com>
2024-07-09 11:51:04 -0600

    added favorite file

---
+++ src/components/AddFavorite.jsx
@@ -0,0 +1,8 @@
+// model Favorite {
+//   id       String   @id @unique @default(uuid())
+//   clientId String
+//   iId      String
+//   pId      String?
+//   iColorId String
+//   pColorId String?
+// }
