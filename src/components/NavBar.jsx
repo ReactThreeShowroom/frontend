c1b25171060802032b05bd5e73b75a355d60abcc
JollyRen <jollren@gmail.com>
2025-03-30 11:24:52 -0600

    NavBar styles, added images for home icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,6 @@ import { useNavigate } from 'react-router-dom'
 import { getNav } from '../utils/maps'
 import { useEffect, useState } from 'react'
 
-// get rid of user after you can fetch user
-// const user = { admin: true, activeSub: true, id: 'asdf' }
-// const user = { admin: false, activeSub: true, id: 'asdf' }
-// const user = { admin: false, activeSub: false, id: 'asdf' }
-// const user = { noUser: true }
-
-// put somewhere else?
 const nav = [
   { value: 'Home', path: '/', dep: [] },
   { value: 'Admin', path: '/admin', dep: ['id', 'admin'] },
@@ -28,23 +21,32 @@ const NavBar = ({ navState: { location, token, setToken, user, setUser } }) => {
 
   return (
     <header id="headContainer" className="flex-col m-0 p-0">
-      <nav id="navContainer" className="flex justify-around">
-        <h1 id="webName" className="font-sans font-semibold text-lg">
-          Kote Master
-        </h1>
-        {linksMap}
-        {!!user.id && (
-          <span
-            onClick={() => {
-              localStorage.setItem('token', '')
-              setToken('')
-              setUser({})
-              navigate('/')
-            }}
-            className={'hover:bg-main-orange hover:text-[#ffffff]'}>
-            Sign Out
-          </span>
-        )}
+      <nav
+        id="navContainer"
+        className="flex flex-col md:flex-row lg:flex-row justify-around items-center">
+        <div
+          className={'flex flex-row items-center my-1 hover:cursor-pointer'}
+          onClick={() => {
+            navigate('/')
+          }}>
+          <img id="homeIconGun" src={'/images/Icon.png'} className="h-[50px] lg:h-[100px] mr-2" />
+          <img id="homeIconKM" src={'/images/KOTEMASTER-Only.svg'} className="h-[28px]" />
+        </div>
+        <div className={'w-1/2 flex flex-row flex-wrap justify-between items-center'}>
+          {linksMap}
+          {!!user.id && (
+            <span
+              onClick={() => {
+                localStorage.setItem('token', '')
+                setToken('')
+                setUser({})
+                navigate('/')
+              }}
+              className={'hover:bg-main-orange hover:text-[#ffffff]'}>
+              Sign Out
+            </span>
+          )}
+        </div>
       </nav>
       {user.id && (
         <div id="infoContainer">
