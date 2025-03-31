f1edf7fece6b3236e87f741d715a13a925fd09f5
JollyRen <jollren@gmail.com>
2025-03-30 19:48:58 -0600

    added some comments about URL

--- src/utils/fetches.js
+++ src/utils/fetches.js
@@ -1,7 +1,8 @@
 import { redirect } from 'react-router'
 
-// export const BASE_URL = 'https://api-1i72.onrender.com'
-export const BASE_URL = 'http://localhost:3000'
+// export const BASE_URL = 'https://api-1i72.onrender.com' //live site
+// export const BASE_URL = 'https://api-3frl.onrender.com' //dev site
+export const BASE_URL = 'http://localhost:3000' // for local dev
 
 export const fetchUserIfToken = async (setter, token) => {
   try {
